f59dd76ea3b3a54273ea8008a2c31d9de4db67c5
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-05-11 02:47:06 +0000

    adicionando projeto ao github

---
+++ mysite/enquetes/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse("tonny weslley silva de souza 20211014040042")
