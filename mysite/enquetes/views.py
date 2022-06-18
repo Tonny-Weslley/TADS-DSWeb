46058f75dc7fad7ae5ee4eb78277c8e3050c7d31
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-06-18 23:38:57 +0000

    atualizando o projeto para modificações locais

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("tonny weslley silva de souza 20211014040042")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'lastest_question_list': lastest_question_list, }
+    return render(request, 'enquetes/index.html', context)
+
+def detail(request, question_id):
+    response = "Você está visualizando a questão %s."
+    return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    response = "Resultados da questão %s."
+    return HttpResponse(response % response)
+
+def vote(request, question_id):
+    response = "Votação da questão %s."
+    return HttpResponse(response % response)
